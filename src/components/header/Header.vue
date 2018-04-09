99dd3ce650a249f396047fc4b046f6c45f8e1a21
alexvndre <alexandre.vallot.ext@francetv.fr>
2018-04-09 16:25:00 +0200

    Settings ok

---
+++ src/components/header/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="#">Dashboard GitLab</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-nav right>
+            <b-nav-item href="#" v-b-modal.modal-settings>Settings</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <settings></settings>
+  </header>
+</template>
+
+<script>
+import Settings from './Settings';
+
+export default {
+  name: 'Header',
+  components: { Settings },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
